8bde2d042dc7bfabac5a40441ea762bd6e7855ee
KuteKetX <kuteketbhop@gmail.com>
2019-09-04 19:04:04 -0600

    Create real chat server and client

--- simple-client.py
+++ simple-client.py
@@ -1,9 +1,28 @@
+import sys
+import select
 import socket
 
-Socket = socket.socket()
-Host = socket.gethostname()
-Port = 1337
+Server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-Socket.connect((Host, Port))
-print Socket.recv(1024)
-Socket.close()
+if len(sys.argv) != 3:
+    print "Usage: simple-client HOST PORT"
+    exit()
+
+Host = str(sys.argv[1])
+Port = int(sys.argv[2])
+Server.connect((Host, Port))
+
+while True:
+    SocketsList = [sys.stdin, Server]
+    ReadSockets, WriteSocket, ErrorSocket = select.select(SocketsList, [], [])
+    for Socks in ReadSockets:
+        if Socks == Server:
+            Message = Socks.recv(2048)
+            print Message
+        else:
+            Message = sys.stdin.readline()
+            Server.send(Message)
+            sys.stdout.write("[You] ")
+            sys.stdout.write(Message)
+            sys.stdout.flush()
+server.close()
--- simple-server.py
+++ simple-server.py
@@ -1,15 +1,54 @@
-#!/usr/bin/python
+import sys
+import select
 import socket
+from thread import *
 
-Socket = socket.socket()
-Host = socket.gethostname()
-Port = 1337
-Socket.bind((Host, Port))
+Server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+Server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-Socket.listen(5)
+if len(sys.argv) != 3:
+    print "Usage: simple-server HOST PORT"
+    exit()
+
+Host = str(sys.argv[1])
+Port = int(sys.argv[2])
+Server.bind((Host, Port))
+Server.listen(100)
+ListOfClients = []
+
+def Clientthread(Conn, Addr):
+    Conn.send("Welcome to my simple chatroom!")
+    while True:
+            try:
+                Message = Conn.recv(2048)
+                if Message:
+                    print "[" + Addr[0] + "] " + Message
+                    MessageToSend = "[" + Addr[0] + "] " + Message
+                    Broadcast(MessageToSend, Conn)
+
+                else:
+                    Remove(Conn)
+            except:
+                continue
+
+def Broadcast(Message, Connection):
+    for Clients in ListOfClients:
+        if Clients != Connection:
+            try:
+                Clients.send(Message)
+            except:
+                Clients.close()
+                Remove(Clients)
+
+def Remove(Connection):
+    if Connection in ListOfClients:
+        ListOfClients.remove(Connection)
 
 while True:
-	Connection, Address = Socket.accept()
-	print 'Got stinky connection from', Address
-	Connection.send('you stinky disconnect from me nerd')
-	Connection.close()
+    Conn, Addr = Server.accept()
+    ListOfClients.append(Conn)
+    print Addr[0] + " connected"
+    StartNewThread(ClientThread, (Conn, Addr))
+
+Conn.close()
+Server.close()
