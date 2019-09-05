b5b2d2c8370e2ce25b431154a68cc0a948f84e90
KuteKetX <kuteketbhop@gmail.com>
2019-09-04 19:04:30 -0600

    Added parameter checks

--- simple-server.py
+++ simple-server.py
@@ -10,8 +10,16 @@ if len(sys.argv) != 3:
     print "Usage: simple-server HOST PORT"
     exit()
 
-Host = str(sys.argv[1])
-Port = int(sys.argv[2])
+if len(sys.argv[1]) < 7:
+	exit("Invalid Host!")
+else:
+	Host = str(sys.argv[1])
+
+if len(sys.argv[2]) < 1:
+	Port = 1337
+else:
+	Port = int(sys.argv[2])
+
 Server.bind((Host, Port))
 Server.listen(100)
 ListOfClients = []
