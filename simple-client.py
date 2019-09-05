6e3f591b8dbe5a7efe7629eec01c74684cdb863c
KuteKetX <kuteketbhop@gmail.com>
2019-09-04 19:09:44 -0600

    Added parameter checks for simple-client

--- simple-client.py
+++ simple-client.py
@@ -8,8 +8,16 @@ if len(sys.argv) != 3:
     print "Usage: simple-client HOST PORT"
     exit()
 
-Host = str(sys.argv[1])
-Port = int(sys.argv[2])
+if len(sys.argv[1]) < 7:
+	exit("Invalid Host!")
+else:
+	Host = str(sys.argv[1])
+
+if len(sys.argv[2]) < 1:
+	Port = 1337
+else:
+	Port = int(sys.argv[2])
+
 Server.connect((Host, Port))
 
 while True:
